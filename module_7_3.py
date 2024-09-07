f37795807ccf2c24ea7c51a0518b43cbc0c354c0
b.smirnov <besm1@yandex.ru>
2024-09-07 22:04:12 +0300

    окончательная версия

--- module_7_3.py
+++ module_7_3.py
@@ -6,18 +6,16 @@ class WordsFinder:
     def __init__(self, *f_names: str):
         self.file_names = f_names
 
-    def __str__(self):
-        return str(self.file_names)
-
     def get_all_words(self):
         all_words = {}
         for f_name_ in self.file_names:
             all_words[f_name_] = []
             with (open(f_name_, 'r', encoding='utf-8') as file):
-                str_ = file.read().lower().translate(str.maketrans(',/=!?;:\n', '        ')
-                                                     ).replace(' - ', ' ').replace('  ', ' ').strip(' ')
+                str_ = file.read().translate(str.maketrans(',/=!?;:\n', '        ')
+                        ).replace(' - ', ' ').replace('  ', ' ').strip(' ').lower()
                 for w_ in str_.split(' '):
-                    all_words[f_name_].append(w_)
+                    if w_ != '':  # Пустые строки появляются от нескольких подряд идущих пробелов
+                        all_words[f_name_].append(w_)
         return all_words
 
     def find(self, word):
@@ -38,19 +36,14 @@ class WordsFinder:
         return out_dic
 
 
-# finder2 = WordsFinder('test_file.txt')
-# print(finder2.get_all_words())
-#
-# print(finder2.find('TEXT'))
-# print(finder2.count('teXT'))
-
 # finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
 #                       'Rudyard Kipling - If.txt',
 #                       'Mother Goose - Monday’s Child.txt')
-finder1 = WordsFinder('Rudyard Kipling - If.txt')
-print(finder1.get_all_words())
-
-print(len(list(finder1.get_all_words()['Rudyard Kipling - If.txt'])))
-#
+# print(finder1.get_all_words())
 # print(finder1.find('the'))
 # print(finder1.count('the'))
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
